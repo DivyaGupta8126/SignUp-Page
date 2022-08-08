d3711ede8af20f06c284f935a0cb2bf3b25134e1
Divya Gupta <divyagupta8126@gmail.com>
2022-08-08 22:06:48 +0530

    Updates

---
+++ precache-manifest.5a5fa2243d6a8b18e835c0fec822abf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "332acfd0512daa288765",
+    "url": "/SignUp-Page/static/css/main.11d64e4e.chunk.css"
+  },
+  {
+    "revision": "332acfd0512daa288765",
+    "url": "/SignUp-Page/static/js/main.332acfd0.chunk.js"
+  },
+  {
+    "revision": "859aa35335c9e82cde47",
+    "url": "/SignUp-Page/static/js/1.859aa353.chunk.js"
+  },
+  {
+    "revision": "d3ba2a5d57a06b7a2662",
+    "url": "/SignUp-Page/static/js/runtime~main.d3ba2a5d.js"
+  },
+  {
+    "revision": "c32b444b3e24b4c1a9e58c481b1d148c",
+    "url": "/SignUp-Page/static/media/courier.c32b444b.png"
+  },
+  {
+    "revision": "e5aca94b172e43f4c9e7777068c616ca",
+    "url": "/SignUp-Page/index.html"
+  }
+];
